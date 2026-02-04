af5f2ecad91bf7c65fa4b3f160c12413562468cd
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-02-04 13:49:38 +0100

    dithering

--- EXOS.md
+++ EXOS.md
@@ -19,7 +19,7 @@
         - [⭐⭐⭐⭐ Mosaïque miroir ✅](#-mosaïque-miroir)
     - [⭐⭐⭐ Glitch ✅](#-glitch)
     - [⭐⭐⭐ Tri de pixels ✅](#-tri-de-pixels)
-    - [⭐⭐⭐(⭐) Fractale de Mandelbrot 🛑](#-fractale-de-mandelbrot)
+    - [⭐⭐⭐(⭐) Fractale de Mandelbrot ✅](#-fractale-de-mandelbrot)
     - [⭐⭐⭐(⭐) Dégradés dans l'espace de couleur Lab ✅](#-dégradés-dans-lespace-de-couleur-lab)
     - [⭐⭐⭐(⭐) Tramage 🛑](#-tramage)
     - [⭐⭐⭐(⭐) Normalisation de l'histogramme ✅](#-normalisation-de-lhistogramme)
@@ -83,10 +83,7 @@ int main()
     for (glm::vec3& color : image.pixels())
     {
         float c = color.r * 0.3 + color.g * 0.59 + color.b * 0.11;
-
-        color.r = c;
-        color.g = c;
-        color.b = c;
+        color = glm::vec3{c}
     }
     image.save("output/grayscale.png");
 }
@@ -669,7 +666,81 @@ int main()
 ## ⭐⭐⭐(⭐) Tramage
 
 ```cpp
-//TODO: ⭐⭐⭐(⭐) Tramage
+#include <sil/sil.hpp>
+#include <cmath>
+#include <vector>
+
+using bayerT = std::vector<std::vector<float>>;
+
+bayerT generateBayerLevel(int n) {
+    if (n == 1) {
+        //bayer at level 0
+        return {
+            { -0.5f,  0.5f },
+            {  1.5f, -1.5f }
+        };
+    }
+
+    bayerT prev = generateBayerLevel(n - 1);
+    int size = prev.size();
+    int newSize = size * 2;
+
+    bayerT result(newSize, std::vector<float>(newSize));
+
+    for (int y = 0; y < size; ++y) {
+        for (int x = 0; x < size; ++x) {
+            float v = prev[y][x];
+
+            //recursion
+            result[y][x] = 4.0f * v + 0.0f;
+            result[y][x + size] = 4.0f * v + 2.0f;
+            result[y + size][x] = 4.0f * v + 3.0f;
+            result[y + size][x + size] = 4.0f * v + 1.0f;
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    //config
+    float trs = 0.675f; //threshold shift [0;1] (the higher, the darker)
+    int level = 2; //bayer level
+
+    //1 : grayscale with sqrt(r^2 + g^2 + b^2)
+    sil::Image image{"images/photo.jpg"};
+    for (glm::vec3& color : image.pixels())
+    {
+        float c = color.r * 0.3 + color.g * 0.59 + color.b * 0.11;
+
+        color = glm::vec3{c};
+    }
+
+    //2 : dithering
+    const auto bayer = generateBayerLevel(level);
+    float maxB = pow(bayer.size(), 2);
+
+    for (int x = 0; x < image.width(); ++x)
+    {
+        for (int y = 0; y < image.height(); ++y) {
+
+            const float brt = image.pixel(x,y).r;
+
+            //bayer value
+            float b = bayer[y % bayer.size()][x % bayer.size()];
+            b = (b + 0.5f) / maxB; //normalize to [0,1]
+
+            const float c = brt > trs - b
+                ? 1.0
+                : 0.0;
+
+            image.pixel(x,y) = glm::vec3{c};
+        }
+
+    }
+
+    image.save("output/dithering.png");
+}
 ```
 
 ## ⭐⭐⭐(⭐) Normalisation de l'histogramme
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,77 @@
 #include <sil/sil.hpp>
-#include <complex>
 #include <cmath>
-#include <iostream>
+#include <vector>
 
-int main() {
-    int size = 500;
+using bayerT = std::vector<std::vector<float>>;
 
-    sil::Image image{size, size};
+bayerT generateBayerLevel(int n) {
+    if (n == 1) {
+        //bayer at level 0
+        return {
+            { -0.5f,  0.5f },
+            {  1.5f, -1.5f }
+        };
+    }
 
-    float maxIter = 40.f;
-    float range = 4.f;
-    float shift = 2.f;
+    bayerT prev = generateBayerLevel(n - 1);
+    int size = prev.size();
+    int newSize = size * 2;
 
-    for (int x = 0; x < size; ++x) {
-        for (int y = 0; y < size; ++y) {
+    bayerT result(newSize, std::vector<float>(newSize));
 
-            float cx = ((float)x / size) * range - shift; //[0;1] -> [0;4] -> [-2;2]
-            float cy = ((float)y / size) * range - shift; //[0;1] -> [0;4] -> [-2;2]
+    for (int y = 0; y < size; ++y) {
+        for (int x = 0; x < size; ++x) {
+            float v = prev[y][x];
 
-            std::complex<float> c{
-                cx,
-                cy
-            }; //c
-
-            std::complex<float> z{0.f, 0.f}; //z = 0
-
-            float iter = 0.f;
-            while (std::abs(z) <= 2 && iter < maxIter) {
-                z = z * z + c;
-                ++iter;
-            }
-
-            image.pixel(x,y) = glm::vec3{iter/maxIter}; //col
+            //recursion
+            result[y][x] = 4.0f * v + 0.0f;
+            result[y][x + size] = 4.0f * v + 2.0f;
+            result[y + size][x] = 4.0f * v + 3.0f;
+            result[y + size][x + size] = 4.0f * v + 1.0f;
         }
     }
 
-    image.save("output/fractal.png");
+    return result;
+}
+
+int main() {
+    //config
+    float trs = 0.675f; //threshold shift [0;1] (the higher, the darker)
+    int level = 2; //bayer level
+
+    //1 : grayscale with sqrt(r^2 + g^2 + b^2)
+    sil::Image image{"images/photo.jpg"};
+    for (glm::vec3& color : image.pixels())
+    {
+        float c = color.r * 0.3 + color.g * 0.59 + color.b * 0.11;
+
+        color = glm::vec3{c};
+    }
+
+    image.save("output/dithering_before.png");
+
+    //2 : dithering
+    const auto bayer = generateBayerLevel(level);
+    float maxB = pow(bayer.size(), 2);
+
+    for (int x = 0; x < image.width(); ++x)
+    {
+        for (int y = 0; y < image.height(); ++y) {
+            
+            const float brt = image.pixel(x,y).r;
+
+            //bayer value
+            float b = bayer[y % bayer.size()][x % bayer.size()];
+            b = (b + 0.5f) / maxB; //normalize to [0,1]
+
+            const float c = brt > trs - b
+                ? 1.0 
+                : 0.0;
+
+            image.pixel(x,y) = glm::vec3{c};
+        }
+        
+    }
+    
+    image.save("output/dithering.png");
 }
